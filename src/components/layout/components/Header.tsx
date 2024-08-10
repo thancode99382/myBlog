1d1a875ddfd8e681e98e1d14d8790679990c62f1
thancode99382 <dat99382@donga.edu.vn>
2024-08-10 16:25:14 +0700

    commit temporary dropdown

--- src/components/layout/components/Header.tsx
+++ src/components/layout/components/Header.tsx
@@ -102,7 +102,7 @@ export default function Example() {
                   />
                 </MenuButton>
               </div>
-              <MenuItems
+              {/* <MenuItems
                 transition
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
@@ -130,7 +130,7 @@ export default function Example() {
                     Sign out
                   </a>
                 </MenuItem>
-              </MenuItems>
+              </MenuItems> */}
             </Menu>
           </div>
         </div>
