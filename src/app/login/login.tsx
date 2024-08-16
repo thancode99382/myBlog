f3e3675cdbd77b36d0e0254b8b6e2a48b0f7019e
thancode99382 <dat99382@donga.edu.vn>
2024-08-16 13:36:09 +0700

    feat error when user login wrong

--- src/app/login/login.tsx
+++ src/app/login/login.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FormEvent, SetStateAction } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { signIn } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
@@ -9,10 +9,11 @@ type IProp = {
   setTranslate: Dispatch<SetStateAction<boolean>>;
 };
 export default function Login(prop: IProp) {
+  const [error, setError] = useState(false);
   const { data: session } = useSession();
 
   if (session) {
-   prop.setTranslate(false)
+    prop.setTranslate(false);
   }
 
   // const session = await getServerSession();
@@ -32,9 +33,17 @@ export default function Login(prop: IProp) {
       router.push("/");
       router.refresh();
       prop.setLoader(false);
+    }else{
+      prop.setLoader(false);
+      setError(true)
     }
+
+  
   };
 
+  const handleFocus = () => {
+    setError(false);
+  };
   return (
     <>
       {" "}
@@ -56,6 +65,7 @@ export default function Login(prop: IProp) {
           <div className="font-bold md:text-6xl text-2xl">Đăng nhập </div>
           <div>
             <input
+            onFocus={handleFocus}
               name="email"
               className=" bg-slate-100 focus:outline-none focus:ring-0 py-4 px-4 rounded-lg w-52 md:w-96 "
               placeholder="Nhập email"
@@ -64,12 +74,16 @@ export default function Login(prop: IProp) {
           </div>
           <div>
             <input
+            onFocus={handleFocus}
               name="password"
               className=" bg-slate-100 focus:outline-none focus:ring-0 py-4 px-4 rounded-lg w-52 md:w-96 "
               placeholder="Nhập mật khẩu"
               type="password"
             />
           </div>
+          {error && (
+            <div className="text-red-600">Mật khẩu hoặc email không đúng</div>
+          )}
           <button
             type="submit"
             className="border px-8 py-2 rounded-lg  text-white   transition-all ease-in-out duration-200 bg-[#533BAD] "
